12ec0b93a92d0066b71e71ce0e38fe25ae90f184
paulAlexSerban <paul.alex.serban@gmail.com>
2021-11-26 13:50:38 +0200

    dt_02 | timestamp comparison and date-fns instalation

--- sandbox.js
+++ sandbox.js
@@ -1,32 +1,19 @@
-const now = new Date();
+const clock = document.querySelector('.clock');
 
-console.log('getFullYear', now.getFullYear());
-console.log('getMonth', now.getMonth());
-console.log('getDate', now.getDate()); // returns the date of the day of the week
-console.log('getDay', now.getDay()); // returns the index of the day in the week
-console.log('getHours', now.getHours()); // returns the number of hours
-console.log('getMinutes', now.getMinutes()); // returns the number of minutes
-console.log('getSeconds', now.getSeconds()); // returns the number of seconds
+const tick = () => {
+  const now = new Date();
 
-// timestamps
-// -> these are the number of milliseconds since January 1, 1970
-// -> very useful to compare two dates together
-console.log('timestamps', now.getTime());
+  const h = now.getHours();
+  const m = now.getMinutes();
+  const s = now.getSeconds();
 
-// date strings
-console.log('date', now.toDateString());
-console.log('time', now.toTimeString());
-console.log('local', now.toLocaleDateString());
+  console.log(`${h} : ${m} : ${s}`);
 
-// timestamp comparison
-const before = new Date('February 1 2021 7:30:59');
+  const html = `
+    <span>${h}</span> : <span>${m}</span> : <span>${s}</span>
+  `;
 
-const difference = now.getTime() - before.getTime();
-console.log('difference', difference);
+  clock.innerHTML = html;
+}
 
-const minutes = Math.round(difference / 1000 / 60);
-const hours = Math.round(minutes / 60);
-const days = Math.round(hours / 24);
-
-console.log(minutes, hours, days);
-console.log(`The blog was written ${days} ago.`)
+setInterval(tick, 1000);
