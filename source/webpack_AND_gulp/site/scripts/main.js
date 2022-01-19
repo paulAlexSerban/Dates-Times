fb51ad90486ebef42606750c95b37bb729054f8d
paulAlexSerban <paul.alex.serban@gmail.com>
2022-01-19 09:21:32 +0200

    removed unnecesary gulp and webpack test code

--- source/webpack_AND_gulp/site/scripts/main.js
+++
@@ -1,5 +0,0 @@
-import * as math from './module';
-
-console.log('test');
-console.log(`2π = ${math.sum(math.pi, math.pi)}`);
-math.test();
--- source/webpack_AND_gulp/tasks/index.js
+++
@@ -1,5 +0,0 @@
-import gulp from 'gulp'
-
-import { scripts } from './webpack'
-
-export const build = gulp.series( scripts )
