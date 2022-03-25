5c81c2810207da52b88d3fc689eb26b28fa58330
paulAlexSerban <paul.alex.serban@gmail.com>
2022-03-25 18:35:13 +0200

    added basic webpack fe build

---
+++ lab_experiements/basic-webpack-fe-build/app/build/webpack.common.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const glob = require("glob");
+const loaders = require("./loaders");
+const webpack = require("webpack"); // to access built-in plugins
+const plugins = require("./plugins");
+
+const getEntries = (path) =>
+  glob.sync(path).reduce((acc, item) => {
+    const path = item.split("/");
+    path.pop();
+    const name = path.pop();
+    acc[name] = item;
+    return acc;
+  }, {});
+
+const entries = getEntries("./source/layout/*/*/*.js");
+
+module.exports = {
+  entry: { ...entries },
+  module: {
+    rules: [loaders.JSLoader, loaders.CSSLoader],
+  },
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "scripts/[name].script.js",
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    plugins.ESLintPlugin,
+    plugins.MiniCssExtractPlugin,
+    plugins.StyleLintPlugin,
+  ],
+};
